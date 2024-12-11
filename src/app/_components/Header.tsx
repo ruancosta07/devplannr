0ec4ef837abf292a00b91ca64a1c6fcf307be79f
ruancosta07 <ruancosta.ti0805@gmail.com>
2024-12-11 10:02:34 -0300

    componente de header da home

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Show from "@/components/home/Show";
+import { LayoutDashboard, MoonStar, SunDim } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <header>
+      <div className="container-width flex items-center py-[3rem]">
+        <Link
+          href={"/"}
+          className="flex text-zinc-900 dark:text-zinc-100 items-center gap-[.6rem] text-[2.4rem] font-semibold"
+        >
+          <LayoutDashboard className="w-[2.8rem] h-[2.8rem]"/>
+          DevPlannr
+        </Link>
+        <nav className="ml-auto flex gap-[1rem] items-center">
+          <Link
+            href={"/login"}
+            className="bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100 text-[1.4rem] border dark:border-zinc-800 p-[.8rem] px-[1rem] rounded-[.5rem] font-semibold"
+          >
+            Login
+          </Link>
+          <Link
+            href={"/login"}
+            className="bg-zinc-900 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-900 text-[1.4rem] border border-transparent p-[.8rem] px-[1rem] rounded-[.5rem] font-semibold"
+          >
+            Criar conta
+          </Link>
+          <div>
+            <Show when={theme === "dark"}>
+              <button
+                onClick={() => setTheme("light")}
+                className="hover:dark:bg-zinc-700/30 p-[.8rem] rounded-[.6rem]"
+              >
+                <MoonStar className="w-[2rem] h-[2rem]" />
+              </button>
+            </Show>
+            <Show when={theme === "light"}>
+              <button
+                onClick={() => setTheme("dark")}
+                className="hover:bg-zinc-300/30 p-[.8rem] rounded-[.6rem]"
+              >
+                <SunDim className="w-[2rem] h-[2rem]" />
+              </button>
+            </Show>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
