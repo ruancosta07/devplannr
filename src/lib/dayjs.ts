81c9682b2c0fcd5aff0e25fbcdc2759b1a3be22c
ruancosta07 <ruancosta.ti0805@gmail.com>
2024-12-17 09:21:32 -0300

    refactor: config de timezone, utc e tempo relativo do dayjs

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -3,10 +3,13 @@ import ptBr from "dayjs/locale/pt-br"
 import utc from "dayjs/plugin/utc"
 import timezone from "dayjs/plugin/timezone"
 import customParse from "dayjs/plugin/customParseFormat"
+import relativeTime from "dayjs/plugin/relativeTime"
+
 const dayjsUtils = dayjs
 dayjsUtils.locale(ptBr)
 dayjsUtils.extend(utc)
 dayjsUtils.extend(timezone)
 dayjsUtils.extend(customParse)
+dayjsUtils.extend(relativeTime)
 
 export default dayjsUtils
