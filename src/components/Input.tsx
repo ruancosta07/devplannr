994f4dd77147916de9aeed6bb25fed7f4b052c68
ruancosta07 <ruancosta.ti0805@gmail.com>
2024-12-17 09:25:21 -0300

    refactor: spread de props e retorno de elementos dependendo do tipo

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ const InputContainer = ({ className, children }: { className?: string; children:
   return <div className={twMerge(className)}>{children}</div>;
 };
 
-const InputLabel = ({ id, label, className }: { id: string; label: string; className?: string }) => {
+const InputLabel = ({ id, label, className }: { id?: string; label: string; className?: string }) => {
   return (
     <label htmlFor={id} className={twMerge("text-[2rem] font-semibold text-zinc-900 dark:text-zinc-100", className)}>
       {label}
@@ -21,11 +21,12 @@ const Input = ({
   onChange,
   id,
   className,
+  ...props
 }: {
   type: InputType;
   value: string;
   onChange?: (value: ChangeEvent<HTMLInputElement>) => void;
-  id: string;
+  id?: string;
   className?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>) => {
   if (type !== "textarea") {
@@ -33,7 +34,7 @@ const Input = ({
       <input
         id={id}
         className={twMerge(
-          "w-full p-[1rem] rounded-[.6rem] border outline-none bg-zinc-100 border-zinc-300 hover:bg-zinc-200 focus:bg-zinc-200 mt-[.4rem] text-[1.4rem] duration-300 ease-in-out dark:bg-zinc-900 dark:border-zinc-800 hover:dark:bg-zinc-800/70 focus:dark:bg-zinc-800/70",
+          "w-full p-[1rem] rounded-[.6rem] border outline-none bg-zinc-100 border-zinc-300 hover:bg-zinc-200 focus:bg-zinc-200 mt-[.4rem] text-[1.4rem] duration-300 ease-in-out dark:bg-zinc-900 dark:border-zinc-800 hover:dark:bg-zinc-800/70 focus:dark:bg-zinc-800/70 ",
           className
         )}
         onChange={(el:ChangeEvent<HTMLInputElement>) => {
@@ -43,6 +44,26 @@ const Input = ({
         }}
         type={type}
         value={value}
+        {...props}
+      />
+    );
+  }
+  if(type === "textarea"){
+    return (
+      <textarea
+        id={id}
+        className={twMerge(
+          "w-full p-[1rem] rounded-[.6rem] border outline-none bg-zinc-100 border-zinc-300 hover:bg-zinc-200 focus:bg-zinc-200 mt-[.4rem] text-[1.4rem] duration-300 ease-in-out dark:bg-zinc-900 dark:border-zinc-800 hover:dark:bg-zinc-800/70 focus:dark:bg-zinc-800/70 resize-y min-h-[10rem] max-h-[16rem]  ",
+          className
+        )}
+        // rows={1.5}
+        onChange={(el:ChangeEvent<HTMLInputElement>) => {
+          if (onChange) {
+            onChange(el);
+          }
+        }}
+        value={value}
+        {...props}
       />
     );
   }
